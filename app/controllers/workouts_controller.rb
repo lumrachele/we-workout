750b4744750291d5361059cbcbb28a2ccd45c605
lumrachele <rlum894@gmail.com>
2018-12-26 17:49:14 -0500

    built out user and workout views

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,25 @@
 class WorkoutsController < ApplicationController
+
+  def index
+    @workouts = Workout.all
+  end
+
+  def new
+    @workout = Workout.new
+  end
+
+  def create
+    @workout = Workout.create(workout_params)
+  end
+
+  def show
+    @workout = Workout.find(params[:id])
+  end
+
+private
+
+  def workout_params
+    params.require(:workout).permit(:title, :sets, :notes)
+  end
+
 end
